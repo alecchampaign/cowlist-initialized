e3fb68a4ae1f3837059e962fe7a5e703e93abc71
Hack Reactor Student <student@hackreactor.com>
2019-11-17 13:38:06 -0500

    Rename anticipated controller methods

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 // TODO: write callbacks 
 // TODO: handle other types of request 
-router.get('/api/cows', controller.get);
-router.post('/api/cows', controller.post);
+router.get('/api/cows', controller.retrieve);
+router.post('/api/cows', controller.save);
 
 module.exports = router;
